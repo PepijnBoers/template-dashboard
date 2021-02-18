525a667ba6733a1ab57002f56ce7e705c0bb2964
PepijnBoers <pepijn96@msn.com>
2021-02-18 23:45:31 +0100

    Add Heroku deployment option

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 from . import sidebar
-from .server import app
+from .server import app, server
 
 # Define web app layout
 app.layout = html.Div([dcc.Location(id="url"), sidebar.sidebar(), sidebar.content()])
